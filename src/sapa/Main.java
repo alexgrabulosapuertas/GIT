e4b009284c8ee566ece8c5c9d71e228c9e1dd61b
AlexGrabulosaPuertas <a.grabulosa@sapalomera.cat>
2020-12-18 09:49:09 +0100

    Hello

---
+++ src/sapa/Main.java
@@ -0,0 +1,10 @@
+package sapa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Write your code
+        System.out.println("Hello world!");
+        System.out.println("Hello 2");
+    }
+}
